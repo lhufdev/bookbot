13f45032cc0e755c8a96f86228f5a3712d6ed5f8
Louis H <id@louish.me>
2025-11-21 19:26:05 +0000

    feat: use custom filepath ar runtime

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import sys
+
 from stats import (
     get_book_character_count,
     get_book_word_count,
     get_sorted_character_counts,
 )
-
-FILE_PATH = "books/frankenstein.txt"
 
 
 def get_book_text(filepath):
@@ -22,11 +22,21 @@ def print_sorted_character_counts(sorted_character_counts):
 
 
 def main():
+    FILE_PATH = None
+
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    elif len(sys.argv) == 2:
+        FILE_PATH = sys.argv[1]
+
     book_text = get_book_text(FILE_PATH)
     book_word_count = get_book_word_count(book_text)
     book_character_counts = get_book_character_count(book_text)
     sorted_character_counts = get_sorted_character_counts(book_character_counts)
 
+    print(sys.argv)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {FILE_PATH}...")
     print("----------- Word Count ----------")
